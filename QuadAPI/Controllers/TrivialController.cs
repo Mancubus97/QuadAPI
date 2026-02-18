1664d733f17523447e47e5acb87f2e95f7c21d64
S. D. Maat <silvijndemaat1@gmail.com>
2026-02-18 17:11:56 +0100

    Added TrivialController and made a simple test connection to opentdb

---
+++ QuadAPI/Controllers/TrivialController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using QuadAPI.Models;
+
+using System.Net.Http;
+
+
+namespace QuadAPI.Controllers
+{
+
+
+    [Route("api")]
+    [ApiController]
+    public class TrivialController : Controller
+    {
+        private readonly HttpClient MyHttpClient;
+
+        public TrivialController(HttpClient httpClient)
+        {
+            MyHttpClient = httpClient;
+        }
+
+
+        // GET: api/questions?amount=10
+        [HttpGet("questions")]
+        public IResult GetQuestions([FromQuery] int amount)
+        {
+            var url = $"https://opentdb.com/api.php?amount={amount}";
+            // synchronous call
+            var response = MyHttpClient.GetAsync(url).Result; // block until done
+
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            return Results.Ok(content);
+        }
+
+      
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
--- QuadAPI/Program.cs
+++ QuadAPI/Program.cs
@@ -3,8 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddHttpClient();
+
 
 var app = builder.Build();
 
